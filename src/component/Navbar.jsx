2c114f0d86f88657c9ad3f72ccf7723fcc155e7b
akshayudupa7 <akshayudupa7@gmail.com>
2022-12-22 11:10:44 +0530

    yahoo

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <div className='sticky top-0 border-2 border-b-stone-100 bg-stone-100'>
     <div className='flex flex-start ' >
       <div className='mt-4 pl-4'>
-        <img src='https://www.google.com/logos/doodles/2022/celebrating-maria-telkes-6753651837109453-s.png' alt="google_image"/>
+        <img src='https://s.yimg.com/pv/static/img/yahoo-search-logo-142x37.png' alt="google_image"/>
       </div>
     </div>
     <div className='flex justify-center'>
